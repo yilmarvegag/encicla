2bf5673bc98b22e4498c03958a9554616a2ac954
Yilmar Vega <yilmarvegag@outlook.com>
2025-09-25 18:21:13 -0500

    feat(api): initial api with services base by form

---
+++ services/Encicla/Encicla.Application/Features/Commands/OTPs/SendOTP/SendOtpCommand.cs
@@ -0,0 +1,6 @@
+﻿using Encicla.Application.Abstractions.Messaging;
+
+namespace Encicla.Application.Features.Commands.OTPs.SendOTP
+{
+    public sealed record class SendOtpCommand(string Email) : ICommand<bool>;
+}
---
+++ services/Encicla/Encicla.Application/Features/Queries/Neighborhoods/GetNeighborhoodsByMunicipalityId/GetNeighborhoodsByMunicipalityIdQuery.cs
@@ -0,0 +1,7 @@
+﻿using Encicla.Application.Abstractions.Messaging;
+using Encicla.Domain.DTOs;
+
+namespace Encicla.Application.Features.Queries.Neighborhoods.GetNeighborhoodsByMunicipalityId
+{
+    public sealed record GetNeighborhoodsByMunicipalityIdQuery(int MunicipalityId) : IQuery<IReadOnlyList<NeighborhoodDto>>;
+}
